5d5449d49460b44b17119811c1518f8b4f83c6b7
Laura Marques <122318912+laur3te@users.noreply.github.com>
2024-09-12 11:23:11 -0300

    Delete STRINGS directory
    Ajustes 12/09

--- STRINGS/fateamento_string.py
+++
@@ -1,9 +0,0 @@
-nome = "Laura Marques de Oliveira"
-
-print(nome [0])
-print(nome [:5]) #Start vazio = considera até a posição 5
-print(nome [6:]) #Fim vazio = considera apartir do 10 até o fim da string
-print(nome [6:14]) #Substring = considera de um index de início até o index de fim
-print(nome [6:14:2]) #Substring com step = igual o anterior, porém só se altera o como um intervalo para impressão das informações.
-print(nome [::-1]) #Inverso
-print(nome [:]) #Sem paramêtro
